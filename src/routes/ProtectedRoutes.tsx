e4919abd59eaa462c07388299d78695ee2b34f6f
jeremie.tergemina <jeremie.tergemina@egs.school>
2023-03-08 18:12:08 +0100

    feat: :sparkles: init all component and page you need

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,1 +1,16 @@
-export {};
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from '../pages/auth/login.page'
+import RegisterPage from '../pages/auth/register.page'
+import HomePage from '../pages/home.page'
+
+export default function ProtectedRoutes() {
+  return (
+    <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/auth/login" element={<LoginPage/>}></Route>
+        <Route path="/auth/sign" element={<RegisterPage/>}></Route>
+    </Routes>
+  )
+}
+
