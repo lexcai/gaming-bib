f7ae328dc143c85f7e1b7a2e70065f5ce6e5c0b5
jeremie.tergemina <jeremie.tergemina@egs.school>
2023-03-08 23:34:34 +0100

    feat: :sparkles: add login/register page + ui integration

--- src/components/auth/Loginform.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loginform = () => {
-  return (
-    <div>Loginform</div>
-  )
-}
-
-export default Loginform;
--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import LoginPage from '../pages/auth/login.page'
-import RegisterPage from '../pages/auth/register.page'
-import HomePage from '../pages/home.page'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../components/auth/login";
+import Register from "../components/auth/register";
+import AuthPage from "../pages/auth/auth.page";
+import HomePage from "../pages/home.page";
 
 export default function ProtectedRoutes() {
   return (
     <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="/auth/login" element={<LoginPage/>}></Route>
-        <Route path="/auth/sign" element={<RegisterPage/>}></Route>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/auth" element={<AuthPage />}>
+        <Route path="/auth/login" element={<Login />}></Route>
+        <Route path="/auth/sign" element={<Register />}></Route>
+      </Route>
     </Routes>
-  )
+  );
 }
-
