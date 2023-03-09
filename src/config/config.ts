9d328cfb67ac13ad27b98776105ca89898900949
lexcai <boudeau.axel24@gmail.com>
2023-03-09 18:09:11 +0100

    why: add userContext

--- src/config/config.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiKey: 'AIzaSyBNoerhzqpqWEDIN5zE5UmoBqU_mZUBxqc',
-  authDomain: 'gaming-bib.firebaseapp.com',
-  projectId: 'gaming-bib',
-  storageBucket: 'gaming-bib.appspot.com',
-  messagingSenderId: '59905086508',
-  appId: '1:59905086508:web:4ce2aad6abc1a113e32d89'
-}
-export default config
--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,39 +1,37 @@
-import { createContext, useState, useEffect} from 'react'
+import { createContext, useState, useEffect, JSXElementConstructor, ReactElement, ReactFragment, ReactPortal} from 'react'
 
-export const UserContext = createContext()
+export const UserContext = createContext<any>(null)
 
-export function UserContextProvider(props) {
+export function UserContextProvider(props: { children: React.ReactNode }) {
+  const [modalState, setModalState] = useState({
+    signUpModal: false,
+    signInModal: false,
+  })
 
-    const [modalState, setModalState] = useState({
-      signUpModal: false,
-      signInModal: false,
-    })
-
-    const toggleModals = modal => {
-        if (modal === 'signIn') {
-            setModalState({
-                signUpModal: false,
-                signInModal: true
-            })
-        }
-        if (modal === 'signUp') {
-                    
-            setModalState({
-                signUpModal: true,
-                signInModal: false
-            })
-        }
-        if (modal === "close") {
-             setModalState({
-                signUpModal: false,
-                signInModal: false,
-          })
-        }
+  const toggleModals = (modal: string) => {
+    if (modal === "signIn") {
+      setModalState({
+        signUpModal: false,
+        signInModal: true,
+      })
     }
+    if (modal === "signUp") {
+      setModalState({
+        signUpModal: true,
+        signInModal: false,
+      })
+    }
+    if (modal === "close") {
+      setModalState({
+        signUpModal: false,
+        signInModal: false,
+      })
+    }
+  }
 
-    return (
-        <UserContext.Provider value={{modalState, toggleModals}}>
-            {props.children}
-        </UserContext.Provider>
-    )
+  return (
+    <UserContext.Provider value={{ modalState, toggleModals }}>
+      {props.children}
+    </UserContext.Provider>
+  )
 }
