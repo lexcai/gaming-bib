c7ef57ea4ec514e4479582604e34fcb738c29ec0
lexcai <boudeau.axel24@gmail.com>
2023-03-09 19:54:17 +0100

    why: WIP link usercontext and more

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ProtectedRoutes from './routes/ProtectedRoutes';
 
 function App() {
--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,46 +1,55 @@
-import React from "react";
-
+import { useContext } from 'react'
+import { UserContext } from '../../context/userContext';
+ 
 const Login = () => {
+
+  const {modalState, toggleModals} = useContext(UserContext)
+  console.log(modalState, toggleModals)
+
   document.title = "Gaming Library - Connexion";
 
   return (
-    <div className="Login">
-      <div className="Login__Description">
-        <h1>Lorem ipsum dolor sit amet</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          perferendis dolorum, dolores placeat corporis minus aliquid reiciendis
-          quaerat quae sint perspiciatis aperiam quidem atque optio nam alias,
-          maiores unde quis?
-        </p>
+    <>
+    {/* {modalState.login && ( */}
+      <div className="Login">
+        <div className="Login__Description">
+          <h1>Lorem ipsum dolor sit amet</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            perferendis dolorum, dolores placeat corporis minus aliquid
+            reiciendis quaerat quae sint perspiciatis aperiam quidem atque optio
+            nam alias, maiores unde quis?
+          </p>
+        </div>
+        <form className="Login__Form">
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Adresse Mail"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Mot de passe"
+            />
+          </div>
+          <button type="button" className="btn btn-primary mb-3">
+            <span>Se connecter</span>
+            <i className="bi bi-arrow-right bi-2x"></i>
+          </button>
+          <div className="Login__ForgotPassword">
+            <p>Mot de passe oublié ?</p>
+          </div>
+        </form>
       </div>
-      <form className="Login__Form">
-        <div className="mb-3">
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="Adresse Mail"
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Mot de passe"
-          />
-        </div>
-        <button type="button" className="btn btn-primary mb-3">
-          <span>Se connecter</span>
-          <i className="bi bi-arrow-right bi-2x"></i>
-        </button>
-        <div className="Login__ForgotPassword">
-          <p>Mot de passe oublié ?</p>
-        </div>
-      </form>
-    </div>
-  );
+    {/* )} */}
+    </>
+  )
 };
 
 export default Login;
