24d31e076395e353bf4de13b345fa15eb3cacaf1
jeremie.tergemina <jeremie.tergemina@egs.school>
2023-03-11 18:19:38 +0100

    fix: :bug: fix gamecontext

--- src/context/gameContext.tsx
+++ src/context/gameContext.tsx
@@ -4,48 +4,48 @@ import Game from "../assets/utils/models/Game"
 export const GameContext = createContext<any>(null)
 
 export function GameContextProvider(props: { children: React.ReactNode }) {
-    const [loadingData, setLoadingData] = useState(true)
-    const [games, setGames] = useState<Game[]>([])
+  const [loadingData, setLoadingData] = useState(true)
+  const [games, setGames] = useState<Game[]>([])
 
-    const gameListProvider = (query: string, gameObj: object): object =>
-      fillList(query, gameObj)
+  const gameListProvider = (query: string, gameObj: object): object => {
+    return fillList(query, gameObj);
+  }
 
-
-    const fetchData = async (query: string|null) => {
-      console.log("OK")
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key": "d142297cf2msh024b1853e5b4ee4p11a3b7jsne9bad46a88d0",
-          "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
-        },
-      };
-
-      try {
-        console.log('OK ///')
-        const response = await fetch(
-          "https://free-to-play-games-database.p.rapidapi.com/api/games" + query,
-          options
-        );
-        console.log("OK ///")
-        const result = await response.json();
-        console.log("RESULT OF THE PROMISE ", result);
-        return result;
-      } catch (error) {
-        setLoadingData(false)
-        console.log("KO /// ", error);
-      }
+  const fetchData = async (query: string|null) => {
+    console.log("OK")
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "d142297cf2msh024b1853e5b4ee4p11a3b7jsne9bad46a88d0",
+        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+      },
     };
-  const fillList = async (query: string | null , gamesObj: object) => {
-      const json = await fetchData(query)
-      const games : Game[] = Object.keys(json).map((key) => json[key]);
-      setGames(games);
-      return games
+
+    try {
+      console.log('OK ///')
+      const response = await fetch(
+        "https://free-to-play-games-database.p.rapidapi.com/api/games" + query,
+        options
+      );
+      console.log("OK ///")
+      const result = await response.json();
+      console.log("RESULT OF THE PROMISE ", result);
+      return result;
+    } catch (error) {
+      setLoadingData(false)
+      console.log("KO /// ", error);
     }
-  
+  };
+
+  const fillList = async (query: string | null , gamesObj: object) => {
+    const json = await fetchData(query)
+    const games : Game[] = Object.keys(json).map((key) => json[key]);
+    setGames(games);
+    return games
+  }
 
   return (
-    <GameContext.Provider value={{ gameListProvider }}>
+    <GameContext.Provider value={{ gameListProvider, games }}>
       {!loadingData && props.children}
     </GameContext.Provider>
   )
