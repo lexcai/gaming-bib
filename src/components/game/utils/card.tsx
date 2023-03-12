f73773762c0e1ec249a838117a305e0e9d1d9f38
lexcai <boudeau.axel24@gmail.com>
2023-03-12 02:41:05 +0100

    WIP:construction: add favorite

--- src/components/game/utils/card.tsx
+++ src/components/game/utils/card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , { useState, useEffect }from "react";
 import { Link } from "react-router-dom";
 import "../../../assets/scss/dashboard/dashboard.scss";
 import Game from "../../../assets/utils/models/Game";
@@ -8,13 +8,26 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ game }) => {
+  const [Favoris, setFavoris] = useState("bi bi-plus")
+
+useEffect(() => {
+  const element = document.getElementById("elementId")
+  if (element) {
+    element.addEventListener("click", () => {
+      setFavoris(Favoris === "bi bi-plus" ? "bi bi-heart" : "bi bi-plus")
+    })
+  }
+}, [Favoris])
 
   return (
     <Link className="CardArea__Item" to={"/dashboard/game/" + game.id}>
-        <img src={game.thumbnail} alt="ceci est la fin" />
-        <p>{game.title}</p>
+      <img src={game.thumbnail} alt="ceci est la fin" />
+      <Link className="CardArea__Item" to={"/dashboard/favoris/"}>
+      <i className={Favoris}></i>
+      </Link>
+      <p>{game.title}</p>
     </Link>
-  );
+  )
 };
 
 export default Card;
