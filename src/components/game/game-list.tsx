4dd0bfd972237511162c8eebdd42cb030807ec8d
lexcai <boudeau.axel24@gmail.com>
2023-03-12 01:12:16 +0100

    why: add Search feature auto fill card OK

--- src/components/game/game-list.tsx
+++ src/components/game/game-list.tsx
@@ -1,42 +1,45 @@
-import { useState, useContext, useRef, useEffect } from "react";
-import { GameContext } from "../../context/gameContext";
-import "../../assets/scss/dashboard/dashboard.scss";
-import Game from "../../assets/utils/models/Game";
-import Card from "./utils/card";
+import { useState, useContext, useRef, useEffect } from "react"
+import { GameContext } from "../../context/gameContext"
+import "../../assets/scss/dashboard/dashboard.scss"
+import Game from "../../assets/utils/models/Game"
+import Card from "./utils/card"
 
 const GameList = () => {
-  const { games } = useContext(GameContext);
-  const [resultFound, setresultFound] = useState("");
-  const inputs = useRef<any[]>([]);
-  const searchRef = useRef<any>();
+  const { games } = useContext(GameContext)
 
-  console.log(games);
+  const inputs = useRef<any[]>([])
+  const searchRef = useRef<any>()
+  const [searchResult, setSearchResult] = useState<Game[]>([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+
+  console.log(games)
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setSearchTerm(e.currentTarget.value)
+  }
+
+  useEffect(() => {
+    const filteredData = games.filter((item) => {
+      const regex = new RegExp(searchTerm, "gi")
+      return item.title.match(regex)
+    })
+
+    setSearchResult(filteredData)
+  }, [searchTerm, games])
 
   const addInputs = (el: never) => {
     if (el && !inputs.current.includes(el)) {
-      inputs.current.push(el);
+      inputs.current.push(el)
     }
-  };
+  }
+    document.title = "Gaming Library - Librairie"
 
-  const handleSearch = async (e: any) => {
-    e.preventDefault();
-    try {
-      // const searchInput = await gameListProvider(
-      //   inputs.current[0].value,
-      //   {}
-      // )
-      setresultFound("");
-      // à tester
-      // formRef.current.reset();
-    } catch {
-      setresultFound("Wopsy,aucun jeu n'a été retrouvé");
-    }
-  };
-
-  useEffect(() => {
-    document.title = "Gaming Library - Librairie";
-    console.log(games);
-  }, [games]);
+  // useEffect(() => {
+  //   document.title = "Gaming Library - Librairie"
+  //   console.log(games)
+  // }, [games])
 
   return (
     <div className="GameList">
@@ -45,28 +48,31 @@ const GameList = () => {
       </div>
       <div className="GameList__TopArea">
         <div className="GameList__TopArea__Search">
-          <i
-            className="bi bi-search"
-            onClick={handleSearch}
-            ref={searchRef}
-          ></i>
-          <input
-            ref={addInputs}
-            type="text"
-            placeholder="Que recherchez vous ?"
-          />
+          <i className="bi bi-search" ref={searchRef}></i>
+            <input
+              ref={addInputs}
+              onChange={handleSearch}
+              name="searchTerm"
+              type="text"
+              placeholder="Que recherchez vous ?"
+            />
+            <button type="submit">TEST</button>
         </div>
         <div className="GameList__TopArea__SortBy">
           <i className="bi bi-sliders2-vertical"></i>
         </div>
       </div>
-      <div className="CardArea">
-        {games.map((game: Game) => (
-          <Card key={game.id} game={game} />
-        ))}
-      </div>
+      {searchResult.length > 0 ? (
+        <div className="CardArea">
+          {searchResult.map((game: Game) => (
+            <Card key={game.id} game={game} />
+          ))}
+        </div>
+      ) : (
+        <div>Aucun résultat pour cette recherche.</div>
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default GameList;
+export default GameList
